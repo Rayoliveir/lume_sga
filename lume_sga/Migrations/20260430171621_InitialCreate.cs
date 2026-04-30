90d3580c65c6e0f9b77fb42023341611e8ec5275
MARCELLY RAYLA DA SILVA OLIVEIRA SANTOS <023.958586@universidadesenaicimatec.edu.br>
2026-04-30 14:18:51 -0300

    Criando banco de dados

---
+++ lume_sga/Migrations/20260430171621_InitialCreate.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace lume_sga.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Prioridades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false),
+                    TempoEstimadoHoras = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prioridades", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Setores",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Setores", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Chamados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titulo = table.Column<string>(type: "TEXT", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: false),
+                    SetorId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PrioridadeId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    DataAbertura = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataHoraInicio = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    DataHoraTermino = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    SolucaoAplicada = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chamados", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Chamados_Prioridades_PrioridadeId",
+                        column: x => x.PrioridadeId,
+                        principalTable: "Prioridades",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Chamados_Setores_SetorId",
+                        column: x => x.SetorId,
+                        principalTable: "Setores",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Prioridades",
+                columns: new[] { "Id", "Nome", "TempoEstimadoHoras" },
+                values: new object[,]
+                {
+                    { 1, "Baixa", 48 },
+                    { 2, "Média", 24 },
+                    { 3, "Alta", 4 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chamados_PrioridadeId",
+                table: "Chamados",
+                column: "PrioridadeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chamados_SetorId",
+                table: "Chamados",
+                column: "SetorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Chamados");
+
+            migrationBuilder.DropTable(
+                name: "Prioridades");
+
+            migrationBuilder.DropTable(
+                name: "Setores");
+        }
+    }
+}
--- lume_sga/Program.cs
+++ lume_sga/Program.cs
@@ -4,25 +4,21 @@ using lume_sga.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Configurar o Banco de Dados (Adicione isso!)
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// 2. Registrar o Service de Negócio
 builder.Services.AddScoped<ChamadoService>();
 
 builder.Services.AddControllers();
 
-// Configuração do OpenAPI (Padrão .NET 9)
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure o pipeline de requisições
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    // Se quiser usar a interface visual do Swagger, podemos adicionar depois
+    
 }
 
 app.UseHttpsRedirection();
