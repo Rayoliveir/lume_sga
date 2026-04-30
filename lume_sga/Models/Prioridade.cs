e355a7fc4eb292de37d3013a3b01c7050577e500
MARCELLY RAYLA DA SILVA OLIVEIRA SANTOS <023.958586@universidadesenaicimatec.edu.br>
2026-04-30 13:54:01 -0300

    ADD: Model prioridade

---
+++ lume_sga/Models/Prioridade.cs
@@ -0,0 +1,8 @@
+﻿namespace lume_sga.Models
+{
+    public class Prioridade {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public int TempoEstimadoHoras { get; set; }
+    }
+}
