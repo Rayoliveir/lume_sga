2fab795549996b0042fea9471aab9ff92188328f
MARCELLY RAYLA DA SILVA OLIVEIRA SANTOS <023.958586@universidadesenaicimatec.edu.br>
2026-04-30 14:25:07 -0300

    ADD: Controllers

--- lume_sga/Controllers/SetoresController.cs
+++ lume_sga/Controllers/SetoresController.cs
@@ -1,35 +1,26 @@
-﻿using lume_sga.Data;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using lume_sga.Data;
 using lume_sga.Models;
-using Microsoft.AspNetCore.Mvc;
-
-namespace lume_sga.Controllers
-{
-    public class SetoresController {
-        using Microsoft.AspNetCore.Mvc;
-        using Microsoft.EntityFrameworkCore;
-        using lume_sga.Data;
-        using lume_sga.Models;
 
 namespace lume_sga.Controllers;
 
-    [ApiController]
-    [Route("api/[controller]")]
-    public class SetoresController : ControllerBase
+[ApiController]
+[Route("api/[controller]")]
+public class SetoresController : ControllerBase
+{
+    private readonly AppDbContext _context;
+    public SetoresController(AppDbContext context) => _context = context;
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Setor>>> GetSetores()
+        => await _context.Setores.ToListAsync();
+
+    [HttpPost]
+    public async Task<ActionResult<Setor>> CreateSetor(Setor setor)
     {
-        private readonly AppDbContext _context;
-        public SetoresController(AppDbContext context) => _context = context;
-
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Setor>>> GetSetores()
-            => await _context.Setores.ToListAsync();
-
-        [HttpPost]
-        public async Task<ActionResult<Setor>> CreateSetor(Setor setor)
-        {
-            _context.Setores.Add(setor);
-            await _context.SaveChangesAsync();
-            return Ok(setor);
-        }
+        _context.Setores.Add(setor);
+        await _context.SaveChangesAsync();
+        return Ok(setor);
     }
-}
 }
