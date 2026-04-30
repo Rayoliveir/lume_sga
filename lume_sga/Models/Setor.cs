539829d5823ceb5131a812aed7959c632e9fa653
MARCELLY RAYLA DA SILVA OLIVEIRA SANTOS <023.958586@universidadesenaicimatec.edu.br>
2026-04-30 13:52:34 -0300

    ADD: Model Setor

---
+++ lume_sga/Models/Setor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace lume_sga.Models
+{
+    public class Setor {
+        public int ID { get; set; }
+        public String Nome { get; set; } = string.Empty;
+    }
+}
