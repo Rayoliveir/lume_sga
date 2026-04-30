6f49e902cfa92e8ff8e30393674b1d3f76feeac7
MARCELLY RAYLA DA SILVA OLIVEIRA SANTOS <023.958586@universidadesenaicimatec.edu.br>
2026-04-30 14:13:48 -0300

    ADD: DTOs

---
+++ lume_sga/DTOs/ChamadoReadDto.cs
@@ -0,0 +1,15 @@
+﻿using lume_sga.Models;
+
+namespace lume_sga.DTOs;
+
+public class ChamadoReadDto
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; } = string.Empty;
+    public string SetorNome { get; set; } = string.Empty;
+    public string PrioridadeNome { get; set; } = string.Empty;
+    public StatusChamado Status { get; set; }
+    public DateTime DataAbertura { get; set; }
+    public double HorasDecorridas { get; set; }
+    public bool EstaAtrasado { get; set; }
+}
