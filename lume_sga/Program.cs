8902c9500b7c02440a32fa4a524eef56f13d8fe3
Rayoliveir <160605874+Rayoliveir@users.noreply.github.com>
2026-05-02 14:54:33 -0300

    ATT: Configurando ambiente para iniciar o front

--- lume_sga/Program.cs
+++ lume_sga/Program.cs
@@ -5,31 +5,29 @@ using lume_sga.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Banco de Dados
+// Banco de Dados
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// 2. Services
+// Services
 builder.Services.AddScoped<ChamadoService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
-// 3. Swagger Simplificado (Sem usar o namespace Microsoft.OpenApi.Models)
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// 4. Interface Visual
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Lume SGA v1");
         c.RoutePrefix = string.Empty;
     });
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
